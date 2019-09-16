2a563442459c8b90b4fdaf76d50303a8dc765c62
Vamsikrishna Tamil selvan <krrishvamsi7@gmail.com>
2019-09-15 22:03:04 -0400

    add log modal added :rocket:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
+import AddButton from "./components/layout/AddButton";
+import AddLogModal from "./components/logs/AddLogModal";
 import "./App.css";
 
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
       <SearchBar />
       <div className="container">
         <Logs />
+        <AddButton />
+        <AddLogModal />
       </div>
     </Fragment>
   );
